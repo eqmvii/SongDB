ae32bf9a11d37fde55d6c50a0747b29f6055ffe5
Eric (Martin) Mancini <coelomate@gmail.com>
2018-04-17 19:33:45 -0400

    add inquirer

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var mysql = require('mysql');
+var inquirer = require('inquirer');
+
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -12,11 +15,53 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query('SELECT * FROM songs', function (error, results, fields) {
-    if (error) throw error;
-    console.log('Songs ', results);
-    connection.end();
-});
+function addSongs() {
+    inquirer.prompt([
+        {
+            message: "artist name",
+            name: "artist"
+        }
+        // ,
+        // {
+        //     message: "song title",
+        //     name: "title"
+        // },
+        // {
+        //     message: "genre",
+        //     name: "genre"
+        // }
+    ]).then(answers => {
+        console.log(answers);
+        inquirer.prompt([
+        {
+            message: "another?",
+            type: "list",
+            name: "again",
+            choices: ["YES", "NO" ]
+        }
+        ]).then(moreAnswers => {
+            console.log(moreAnswers);
+            if (moreAnswers.again === "YES") {
+                addSongs();
+            } else {
+                connection.end();
+            }
+        })
+    });
+}
+
+addSongs();
+
+
+
+// connection.query('SELECT * FROM songs', function (error, results, fields) {
+//     if (error) throw error;
+//     console.log('Songs ', results);
+//     connection.end();
+// });
+
+
+
 
 // It worked! Output:
 
